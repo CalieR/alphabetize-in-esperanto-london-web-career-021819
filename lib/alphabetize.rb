69551fba3f76a6dd1f83c004f4badca87d92b258
Calie Rushton <calielrushton@gmail.com>
2019-01-30 16:14:09 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by do |phrase|
-    phrase.split("").map do |char|
+  arr.sort_by do |phrase| # sort the elements in arr by given condition
+    phrase.split("").map do |char| # create new array with each character replaced by it's corresponding index number in esperanto
       esperanto.index(char)
     end
   end
